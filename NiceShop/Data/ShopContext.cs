2815f9ff310f3ffcb0cfd104ee50293da7e26c8e
saramo55 <2401628@student.eelu.edu.eg>
2026-09-11 02:08:30 +0300

    DEV1-01: Add Product and Category entities

---
+++ NiceShop/Data/ShopContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopMvc.Models;
+
+namespace ShopMvc.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ NiceShop/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopMvc.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string SKU { get; set; }
+
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? OriginalPrice { get; set; }
+
+        public int Stock { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public string ImagesJson { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
