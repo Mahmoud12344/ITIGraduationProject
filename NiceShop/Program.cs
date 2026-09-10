b8a4a1c931cd1d83948fff2727a3de37cfff3df7
Mahmoud-Othman <mahmoudmohsen12344@gmail.com>
2026-09-10 19:23:50 +0300

    added the synamic user-secret connection string build

--- NiceShop/Program.cs
+++ NiceShop/Program.cs
@@ -1,12 +1,29 @@
-namespace NiceShop;
+using Microsoft.EntityFrameworkCore;
 
+namespace NiceShop;
+using Microsoft.Data.SqlClient;
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
-
+        var baseConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+        var connectionBuilder = new SqlConnectionStringBuilder(baseConnection);
+        var dbServer = builder.Configuration["DbServer"];
+        var dbUser = builder.Configuration["DbUser"];
+        var dbPassword = builder.Configuration["DbPassword"];
+        if (!string.IsNullOrEmpty(dbServer))
+        {
+            connectionBuilder.DataSource = dbServer; 
+            connectionBuilder.UserID = dbUser;
+            connectionBuilder.Password = dbPassword;
+            connectionBuilder.Encrypt = false;
+            connectionBuilder.TrustServerCertificate = true;
+        }
+        
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionBuilder.ConnectionString));
         // Add services to the container.
         builder.Services.AddControllersWithViews();
-
+    
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
