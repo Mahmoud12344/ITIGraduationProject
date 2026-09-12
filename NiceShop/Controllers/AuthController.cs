651583d87ddc08e876a4316f22a65249ac76bb2f
Mahmoud-Othman <mahmoudmohsen12344@gmail.com>
2026-09-12 09:05:41 +0300

    add the login logic and the signOut

--- NiceShop/Controllers/AuthController.cs
+++ NiceShop/Controllers/AuthController.cs
@@ -1,22 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.VisualBasic;
 using NiceShop.Models;
 using NiceShop.ViewModels;
 
 namespace NiceShop.Controllers;
 
-public class AuthController: Controller {
+// [Authorize]
+public class AuthController : Controller {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
-  public  AuthController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser>signInManager) {
+
+    public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) {
         _userManager = userManager;
         _signInManager = signInManager;
     }
+
     [HttpGet]
-    public IActionResult Index()
-    {
-        var model = new AuthVM
-        {
+    public IActionResult Index() {
+        var model = new AuthVM {
             ActiveTab = "login"
         };
 
@@ -25,10 +29,8 @@ public class AuthController: Controller {
 
 
     [HttpGet]
-    public IActionResult CreateAccount()
-    {
-        var model = new AuthVM
-        {
+    public IActionResult CreateAccount() {
+        var model = new AuthVM {
             ActiveTab = "register"
         };
 
@@ -36,37 +38,66 @@ public class AuthController: Controller {
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateAccount(UserRegistrationVM urvm)
-    {
-        if (!ModelState.IsValid)
-        {
+    public async Task<IActionResult> CreateAccount(UserRegistrationVM urvm) {
+        if (!ModelState.IsValid){
             var authVmInvalid = new AuthVM { Register = urvm, ActiveTab = "register" };
             return View(nameof(Index), authVmInvalid);
         }
 
         var user = new ApplicationUser();
-        user.UserName = urvm.Email; 
+        user.UserName = urvm.Email;
         user.Email = urvm.Email;
         user.PhoneNumber = urvm.Phone;
 
         var res = await _userManager.CreateAsync(user, urvm.Password);
         if (res.Succeeded){
             await _signInManager.SignInAsync(user, false);
-            return RedirectToAction("Index"); 
+            return RedirectToAction("Index");
         }
 
-         foreach (var error in res.Errors)
-        {
+        foreach (var error in res.Errors){
             ModelState.AddModelError("", error.Description);
         }
-    
+
         // Return the View with the data so the user can see the errors 
         var authVmFailed = new AuthVM { Register = urvm, ActiveTab = "register" };
         return View(nameof(Index), authVmFailed);
     }
 
-     
-    
+    [HttpGet]
+    public async Task<IActionResult> SignIn() {
+        return View("Index");
+    }
+
+    [HttpPost]
+
+    public async Task<IActionResult> SaveSignIn(AuthVM authVm) {
+        authVm.ActiveTab = "login";
+        bool loginValid = ModelState
+            .Where(kvp => kvp.Key.StartsWith("Login"))
+            .All(kvp => kvp.Value.ValidationState == ModelValidationState.Valid);
+        if (!loginValid){
+            return View("Index", authVm);
+        }
+
+        var appuser = await _userManager.FindByNameAsync(authVm.Login.Email);
+        if (appuser is null){
+            ModelState.AddModelError("", "Username can't wrong ");
+            return View("Index", authVm);
+        }
+
+        var isPresent = await _userManager.CheckPasswordAsync(appuser,authVm.Login.Password);
+        if (!isPresent){
+            ModelState.AddModelError("", "Wrong Password ");
+            return View("Index", authVm);
+            
+        }
+        
+            await _signInManager.SignInAsync(appuser, authVm.Login.RememberMe);
+ 
+            return RedirectToAction("Index", "Home");
+    }
+
 
     public async Task<IActionResult> SignOut() {
         await _signInManager.SignOutAsync();
