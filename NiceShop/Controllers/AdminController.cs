92e84613f0d9e03acb2e0c8f161a0d4bdb914749
Mahmoud-Othman <mahmoudmohsen12344@gmail.com>
2026-09-12 05:31:27 +0300

    added the checkAuthantication mw in program

--- NiceShop/Controllers/AdminController.cs
+++ NiceShop/Controllers/AdminController.cs
@@ -1,7 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NiceShop.Controllers;
-
+// [Authorize]
 public class AdminController : Controller
 {
     public IActionResult Dashboard()
