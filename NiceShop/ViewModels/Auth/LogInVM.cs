1a1350e3b9ccce424defe2012884dcd5d4759abc
Mahmoud-Othman <mahmoudmohsen12344@gmail.com>
2026-09-12 09:09:18 +0300

    eddit the auth index for the login

--- NiceShop/ViewModels/Auth/LogInVM.cs
+++ NiceShop/ViewModels/Auth/LogInVM.cs
@@ -10,4 +10,6 @@ public class LoginViewModel
     [Required]
     [DataType(DataType.Password)]
     public string Password { get; set; }
+    [Display(Name = "Remember Me ")]
+    public bool RememberMe{ get; set; } = false;
 }
