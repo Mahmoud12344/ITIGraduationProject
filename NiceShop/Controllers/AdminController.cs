fd52a40a5f564ad20e0a4f16b9127056101f8808
Mahmoud-Othman <mahmoudmohsen12344@gmail.com>
2026-09-13 00:16:33 +0300

    edit the admin nav to go to the correct coupon action dashboard

--- NiceShop/Controllers/AdminController.cs
+++ NiceShop/Controllers/AdminController.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NiceShop.Data;
+using NiceShop.Models;
+using NiceShop.ViewModels;
 
 namespace NiceShop.Controllers;
 [Authorize (Roles = "Admin")]
-public class AdminController : Controller
+public class AdminController(ApplicationDbContext dbContext ) : Controller
 {
     public IActionResult Dashboard()
     {
@@ -34,4 +38,12 @@ public class AdminController : Controller
     {
         return View();
     }
+    // public IActionResult Coupons() {
+    //     var cvm = new CouponVm() {
+    //         Coupons = dbContext.Coupons.ToList() 
+    //     };
+    //     
+    //     return View(cvm);
+    // }
+
 }
