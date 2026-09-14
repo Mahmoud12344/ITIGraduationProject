9137b616936c6bcb1e57552b3ba948e871459bbd
saramo55 <2401628@student.eelu.edu.eg>
2026-09-14 07:48:13 +0300

    Fix product filtering, product images display, stock edit bug, and product details page

--- NiceShop/Controllers/ProductsController.cs
+++ NiceShop/Controllers/ProductsController.cs
@@ -19,6 +19,7 @@ public class ProductsController : Controller
         var products = await _context.Products
             .Include(p => p.Category)
             .Include(p => p.Brand)
+            .Include(p => p.Images)
             .Where(p => p.IsActive)
             .ToListAsync();
 
@@ -28,9 +29,35 @@ public class ProductsController : Controller
         return View(products);
     }
 
-    public IActionResult Details(string id)
+    // GET: Products/Details/5
+    public async Task<IActionResult> Details(int id)
     {
-        return View();
+        var product = await _context.Products
+            .Include(p => p.Category)
+            .Include(p => p.Brand)
+            .Include(p => p.Images)
+            .Include(p => p.Colors)
+            .Include(p => p.Sizes)
+            .Include(p => p.Reviews)
+                .ThenInclude(r => r.Customer)
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        var relatedProducts = await _context.Products
+            .Include(p => p.Category)
+            .Include(p => p.Brand)
+            .Include(p => p.Images)
+            .Where(p => p.CategoryId == product.CategoryId && p.Id != id && p.IsActive)
+            .Take(4)
+            .ToListAsync();
+
+        ViewBag.RelatedProducts = relatedProducts;
+
+        return View(product);
     }
 
     // GET: Products/Categories
@@ -52,6 +79,7 @@ public class ProductsController : Controller
         ViewBag.CategoryName = category.Name;
 
         var products = await _context.Products
+            .Include(p => p.Images)
             .Where(p => p.CategoryId == id && p.IsActive)
             .ToListAsync();
 
